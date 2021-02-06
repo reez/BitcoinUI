493ecec36b0b0ecbdd133d83b0d593d26db9057e
Matthew Ramsden <ramsden@hey.com>
2021-02-06 11:50:16 -0600

    format: adding spacing

---
+++ Sources/WalletUI/Format.swift
@@ -0,0 +1,15 @@
+//
+//  Format.swift
+//  
+//
+//  Created by Matthew Ramsden on 2/6/21.
+//
+
+//import Foundation
+import UIKit
+
+extension CGFloat {
+  static func pf_grid_10(_ n: Int) -> CGFloat {
+    return CGFloat(n) * 10
+  }
+}
