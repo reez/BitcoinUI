bdfccfe91460c2a7cc132484ea4dc787f6c3ae68
Matthew Ramsden <ramsden@hey.com>
2021-02-06 11:52:04 -0600

    format: adding spacing vertical+horizontal

--- Sources/WalletUI/Format.swift
+++ Sources/WalletUI/Format.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 extension CGFloat {
-  static func pf_grid_10(_ n: Int) -> CGFloat {
-    return CGFloat(n) * 10
-  }
+    
+    static func pf_grid_10(_ n: Int) -> CGFloat {
+        return CGFloat(n) * 10
+    }
+    
+    static func pf_grid_vertical(_ n: Int) -> CGFloat {
+        return CGFloat(n) * 20
+    }
+    
+    static func pf_grid_horizontal(_ n: Int) -> CGFloat {
+        return CGFloat(n) * 10
+    }
+    
 }
