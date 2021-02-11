741896891b7650765482090b328f6c216ba37a29
Matthew Ramsden <ramsden@hey.com>
2021-02-11 17:20:02 -0600

    mymattress: double bed setup warning view complete

--- Sources/WalletUI/MyMattress/MMDoubleBedIntroView.swift
+++ Sources/WalletUI/MyMattress/MMDoubleBedIntroView.swift
@@ -26,8 +26,7 @@ struct MMDoubleBedIntroView: View {
                     Text("Switch security mode to Triple Bed")
                         .bold()
                         .font(.title)
-                        // This should be label but because we are using set color for light/dark double bed it needs to be gray atm
-                        .foregroundColor(Color(UIColor.white))
+                        .foregroundColor(Color(UIColor.systemBackground))
                         .multilineTextAlignment(.center)
                     
                     Text("Improve your wallet security by setting up 2 hardware devices.")
@@ -90,11 +89,69 @@ struct MMDoubleBedIntroView: View {
     }
 }
 
+struct MMDoubleBedWarningView: View {
+    var body: some View {
+        
+        ZStack {
+            Color(UIColor.bitcoinMyMattress)
+            
+            VStack(spacing: 20.0) {
+                
+                ZStack {
+                    Circle().frame(width: 50.0, height: 50.0).foregroundColor(.purple)
+                    BitcoinImage(named: "Wallet filled big")
+                        .foregroundColor(Color(UIColor.systemBackground))
+                        .font(.footnote)
+                }
+                
+                VStack(spacing: 5.0) {
+                    Text("This will cost a little bit")
+                        .bold()
+                        .font(.title)
+                        .foregroundColor(Color(UIColor.label))
+                        .multilineTextAlignment(.center)
+                    
+                    Text("To switch modes, a new wallet needs to be created. Funds from your old wallet will be transferred over, which incurs transaction fees.")
+                        .font(.title3)
+                        // This should be secondaryLabel but because we are using set color for light/dark double bed it needs to be gray atm
+                        .foregroundColor(Color(UIColor.lightGray))
+                        .multilineTextAlignment(.center)
+                }
+                
+                Spacer()
+                
+                Button(action: {}) {
+                    Text("Continue")
+                        .padding(.horizontal, .wallet_grid_horizontal_10())
+                        .padding(.vertical, .wallet_grid_vertical_20())
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.black)
+                        .background(Color(UIColor.white))
+                }
+                
+            }
+            .padding(.horizontal, .wallet_grid_horizontal_10())
+            .padding(.vertical, .wallet_grid_vertical_20())
+            .padding(.horizontal, .wallet_grid_horizontal_10())
+            .padding(.vertical, .wallet_grid_vertical_20())
+            .padding(.horizontal, .wallet_grid_horizontal_10())
+            .padding(.vertical, .wallet_grid_vertical_20())
+            
+        }
+        .edgesIgnoringSafeArea(.all)
+        
+    }
+}
+
 struct MMDoubleBedSetupView_Previews: PreviewProvider {
     static var previews: some View {
         MMDoubleBedIntroView()
             .environment(\.colorScheme, .light)
         MMDoubleBedIntroView()
+            .environment(\.colorScheme, .dark)
+        MMDoubleBedWarningView()
+            .environment(\.colorScheme, .light)
+        MMDoubleBedWarningView()
             .environment(\.colorScheme, .dark)
     }
 }
