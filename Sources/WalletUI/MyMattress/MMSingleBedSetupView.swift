18622cbf0f9065f7f891fcb4515bbf3cfacace70
Matthew Ramsden <ramsden@hey.com>
2021-02-11 16:48:08 -0600

    mymattress: single bed setup intro view complete

---
+++ Sources/WalletUI/MyMattress/MMSingleBedSetupView.swift
@@ -0,0 +1,77 @@
+//
+//  MMSingleBedSetupView.swift
+//  
+//
+//  Created by Matthew Ramsden on 2/8/21.
+//
+
+import SwiftUI
+
+#if canImport(UIKit)
+
+
+struct MMSingleBedIntroView: View {
+    var body: some View {
+        
+        ZStack {
+            Color(UIColor.systemBackground)
+            
+            VStack(spacing: 20.0) {
+                
+                ZStack {
+                    Circle().frame(width: 50.0, height: 50.0).foregroundColor(.blue)
+                    BitcoinImage(named: "singlebedsettings-group")
+                        .foregroundColor(Color(UIColor.systemBackground))
+                        .font(.footnote)
+                }
+                
+                VStack(spacing: 5.0) {
+                    Text("Switch security mode to Single Bed")
+                        .bold()
+                        .font(.title)
+                        .foregroundColor(Color(UIColor.label))
+                        .multilineTextAlignment(.center)
+                    
+                    Text("A simple security model for lower amounts that allows you to make transactions directly with this phone.")
+                        .font(.title3)
+                        .foregroundColor(Color(UIColor.secondaryLabel))
+                        .multilineTextAlignment(.center)
+                }
+                
+                Spacer()
+                
+                Button(action: {}) {
+                    Text("Continue")
+                        .padding(.horizontal, .wallet_grid_horizontal_10())
+                        .padding(.vertical, .wallet_grid_vertical_20())
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .background(Color(UIColor.bitcoinOrange))
+                }
+                
+            }
+            .padding(.horizontal, .wallet_grid_horizontal_10())
+            .padding(.vertical, .wallet_grid_vertical_20())
+            .padding(.horizontal, .wallet_grid_horizontal_10())
+            .padding(.vertical, .wallet_grid_vertical_20())
+            .padding(.horizontal, .wallet_grid_horizontal_10())
+            .padding(.vertical, .wallet_grid_vertical_20())
+            
+        }
+        .edgesIgnoringSafeArea(.all)
+        
+    }
+}
+
+struct MMTransactionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MMSingleBedIntroView()
+                .environment(\.colorScheme, .light)
+            MMSingleBedIntroView()
+                .environment(\.colorScheme, .dark)
+        }
+    }
+}
+
+#endif
