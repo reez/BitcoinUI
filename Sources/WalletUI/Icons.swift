4e2fbca766a41bd71dd9af76b4e6946d94e85371
Matthew Ramsden <ramsden@hey.com>
2021-06-12 11:33:47 -0500

    docs: add docc for image func

--- Sources/WalletUI/Icons.swift
+++ Sources/WalletUI/Icons.swift
@@ -10,10 +10,29 @@ import SwiftUI
 
 #if canImport(UIKit)
 
+
+/// A function that returns a Bitcoin Icon `Image`
+///
+/// This is similar to `Image` but returns a Bitcoin specific `BitcoinImage`
+///
+/// ```swift
+/// BitcoinImage(named: "coldcard")
+/// ```
+/// - Parameter named: The name of the Bitcoin Icon you would like to return.
+/// - Returns: The  specific Bitcoin `Image`
 public func BitcoinImage(named: String) -> Image {
     return Image(named, bundle: Bundle.module)
 }
 
+/// A function that returns a Bitcoin Icon `UIImage`
+///
+/// This is similar to `UIImage` but returns a Bitcoin specific `BitcoinUIImage`
+///
+/// ```swift
+/// BitcoinUIImage(named: "coldcard")
+/// ```
+/// - Parameter named: The name of the Bitcoin Icon you would like to return.
+/// - Returns: The  specific Bitcoin `UIImage`
 public func BitcoinUIImage(named: String) -> UIImage {
     return UIImage(named: named, in: .module, compatibleWith: nil)!
 }
