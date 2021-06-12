bbd289563772e3e4f8cedaceb58548029f9d27e4
Matthew Ramsden <ramsden@hey.com>
2021-06-12 14:43:26 -0500

    docs: add docc tutorials images

---
+++ Sources/WalletUI/WalletUI.docc/Resources/code-files/01-02-images.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct ImagesTutorial: View {
+    var body: some View {
+        
+        BitcoinImage(named: "coldcard")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 75.0)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagesTutorial()
+    }
+}
