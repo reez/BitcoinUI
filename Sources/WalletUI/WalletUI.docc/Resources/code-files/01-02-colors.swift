f6bc9f92311f3f22afebcc635d23d0a68a7d8997
Matthew <6657488+reez@users.noreply.github.com>
2021-06-13 11:53:46 -0500

    docs: xcode 13 docC
    * swift-tools-version:5.5
    
    * docs: add docc for image func
    
    * docs: add docc articles
    
    * docs: add docc tutorials
    
    * docs: add docc tutorials images
    
    * docs: add docc tutorials previews

---
+++ Sources/WalletUI/WalletUI.docc/Resources/code-files/01-02-colors.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import WalletUI
+
+struct ColorsTutorial: View {
+    var body: some View {
+        
+        Text("Bitcoin Orange")
+            .font(.caption)
+            .foregroundColor(Color(UIColor.bitcoinOrange))
+            .multilineTextAlignment(.center)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorsTutorial()
+    }
+}
---
+++ Sources/WalletUI/WalletUI.docc/Resources/code-files/01-02-images.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import WalletUI
+
+struct ImagesTutorial: View {
+    var body: some View {
+        
+        BitcoinImage(named: "coldcard")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 75.0)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagesTutorial()
+    }
+}
