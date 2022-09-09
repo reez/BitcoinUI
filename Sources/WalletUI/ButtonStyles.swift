0c03da41e23fd4eae5b8849599129ea93c54704d
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-09-09 10:38:42 +0100

    Cleanup

--- Sources/WalletUI/ButtonStyles.swift
+++ Sources/WalletUI/ButtonStyles.swift
@@ -15,6 +15,7 @@ public let defaultTintColor = Color.bitcoinOrange
 public let defaultTextColor = Color.bitcoinWhite
 public let defaultDisabledFillColor = Color.bitcoinNeutral2
 public let defaultDisabledTextColor = Color.bitcoinNeutral5
+public let defaultDisabledOutlineColor = Color.bitcoinNeutral4
 
 /// A `ButtonStyle` corresponding to the Filled type in the Bitcoin Wallet UI Kit
 ///
@@ -74,6 +75,20 @@ public struct BitcoinFilled: ButtonStyle {
     }
 }
 
+/// A `ButtonStyle` corresponding to the Outline type in the Bitcoin Wallet UI Kit
+///
+/// ```swift
+/// Button("Label") {
+///     print("Button pressed!")
+/// }
+///.buttonStyle(BitcoinOutlined())
+/// ```
+/// - Parameter width: The width of the button (optional, default is 315.0)
+/// - Parameter height: The width of the button (optional, default is 48.0)
+/// - Parameter cornerRadius: The corner radius of the button (optional, default is 5.0)
+/// - Parameter tintColor: The border color of the button (optional, default is .bitcoinOrange)
+/// - Parameter disabledColor: The disabled background color of the button (optional, default is .bitcoinNeutral4)
+///
 public struct BitcoinOutlined: ButtonStyle {
     @Environment(\.colorScheme) private var colorScheme
     @Environment(\.isEnabled) private var isEnabled
@@ -84,7 +99,7 @@ public struct BitcoinOutlined: ButtonStyle {
     let tintColor: Color
     let disabledColor: Color
     
-    public init(width: CGFloat = defaultButtonWidth, height: CGFloat = defaultButtonHeight, cornerRadius: CGFloat = defaultCornerRadius, tintColor: Color = defaultTintColor, disabledColor: Color = defaultDisabledTextColor) {
+    public init(width: CGFloat = defaultButtonWidth, height: CGFloat = defaultButtonHeight, cornerRadius: CGFloat = defaultCornerRadius, tintColor: Color = defaultTintColor, disabledColor: Color = defaultDisabledOutlineColor) {
         self.width = width
         self.height = height
         self.cornerRadius = cornerRadius
@@ -115,48 +130,3 @@ public struct BitcoinOutlined: ButtonStyle {
         return isEnabled ? tintColor : disabledColor
     }
 }
-
-
-/// A `ButtonStyle` according to the Outline type in the Bitcoin Wallet UI Kit
-///
-/// ```swift
-/// Button("Label") {
-///     print("Button pressed!")
-/// }
-///.buttonStyle(BitcoinOutlined())
-/// ```
-/// - Parameter width: The width of the button (optional, default is 315.0)
-/// - Parameter cornerRadius: The corner radius of the button (optional, default is 5.0)
-/// - Parameter tintColor: The border color of the button (optional, default is .bitcoinOrange)
-/// - Parameter disabledColor: The disabled background color of the button (optional, default is .bitcoinNeutral4)
-//public struct BitcoinOutlined: ButtonStyle {
-//    @Environment(\.isEnabled) var isEnabled
-//    @Environment(\.colorScheme) var colorScheme
-//    var width = 315.0
-//    var cornerRadius = 5.0
-//    var tintColor = Color.bitcoinOrange
-//    var disabledColor = Color.bitcoinNeutral4
-//
-//    func makeBody(configuration: Configuration) -> some View {
-//        configuration.label
-//            .padding()
-//            .frame(width: width, height: 46)
-//            .background(stateBackgroundColor().cornerRadius(cornerRadius))
-//            .foregroundColor(stateTextColor())
-//            .overlay(
-//                RoundedRectangle(cornerRadius: cornerRadius)
-//                    .stroke(stateBorderColor(configuration: configuration), lineWidth: 1.5)
-//                )
-//            .scaleEffect(configuration.isPressed ? 0.95 : 1)
-//            .animation(.easeOut(duration: 0.1), value: configuration.isPressed)
-//    }
-//    func stateBackgroundColor() -> Color {
-//        return colorScheme == .dark ? .bitcoinBlack : .bitcoinWhite
-//    }
-//    func stateBorderColor(configuration: Configuration) -> Color {
-//        return isEnabled ? configuration.isPressed ? tintColor.opacity(0.8) : tintColor : disabledColor
-//    }
-//    func stateTextColor() -> Color {
-//        return isEnabled ? tintColor : disabledColor
-//    }
-//}
