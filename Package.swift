67b38e710cf9ba3023788c6a711cd134d2147a07
Matthew <6657488+reez@users.noreply.github.com>
2023-08-28 10:53:51 -0500

    format: updated swift-format

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BitcoinUI",
-            targets: ["BitcoinUI"])
+            targets: ["BitcoinUI"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +24,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BitcoinUI",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "BitcoinUITests",
-            dependencies: ["BitcoinUI"]),
+            dependencies: ["BitcoinUI"]
+        ),
     ]
 )
 
