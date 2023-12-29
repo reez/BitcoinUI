65690af03588a9efc91bf12160f3cc255088a35b
Matthew <6657488+reez@users.noreply.github.com>
2023-12-29 13:26:19 -0600

    version: bump minimum to swift-tools-version-5.7 and ios16

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BitcoinUI",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -32,10 +32,3 @@ let package = Package(
         ),
     ]
 )
-
-#if swift(>=5.6)
-    // Add the documentation compiler plugin if possible
-    package.dependencies.append(
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-    )
-#endif
